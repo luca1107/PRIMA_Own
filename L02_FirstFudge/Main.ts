0b80482dbc1a84ea330ece256252b99b20d047bf
luca bösinger <lb@imsimity.de>
2019-10-15 15:49:28 +0200

    Update

--- L02_FirstFudge/Main.ts
+++ L02_FirstFudge/Main.ts
@@ -1,5 +1,5 @@
 namespace L02_FirstFudge {
-    import ƒ = FudgeCore;
+    import f = FudgeCore;
     
     window.addEventListener("load", hndLoad);
     
@@ -7,8 +7,8 @@ namespace L02_FirstFudge {
         const canvas: HTMLCanvasElement = document.querySelector("canvas");
         console.log(canvas);
         
-        let viewport: ƒ.Viewport = new ƒ.Viewport();
-        viewport.initialize("Viewport", null, null,canvas);
-
+        let viewport: f.Viewport = new f.Viewport();
+        viewport.initialize("Viewport", null, null, canvas);
+  
     }
 }
